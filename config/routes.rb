3f8b035aa8cb4c7fd250af0b23df161fb67e27b4
h <710543268@qq.com>
2022-08-09 17:04:36 +0800

    修改路由

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :validation_codes, only: [:create]
-      resource: :session, only: [:create,:destroy]
-      resource: :me, only: [:show]
+      resource  :session, only: [:create,:destroy]
+      resource  :me, only: [:show]
       resources :items
       resources :tags
     end
   end
-      
+
 end
