1d696ab85e1f7a7e9df9ff769c5785cf9cd82a88
h <710543268@qq.com>
2022-08-10 23:04:51 +0800

    测试

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it '有 email' do
-    user = User.new email: 'han@1.com'
-    expect(user.email).to eq 'han@1.com'
+    user = User.new email: 'frank@1.com'
+    expect(user.email).to eq 'frank@1.com'
   end
 end
